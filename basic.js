f844e21b781d38c88e2dd5b1fc91824032753992
aakash <aakashjaiswal90261@gmail.com>
2025-03-28 13:34:23 +0530

    basic

---
+++ basic.js
@@ -0,0 +1,46 @@
+
+console.log("before my if statment");
+let age=17;
+if(age>=18){
+    console.log("you can vote");
+    console.log("you can drive");
+    
+}else if(age>=16){
+    console.log("you can drive to learing liscence");
+    
+}
+
+
+let marks =90;
+if (marks >= 33 & marks>=80){
+    console.log("pass");
+    console.log("A");
+    
+    
+}
+
+
+let str ="apple";
+if(str[0]==='a' && str.length >3){
+    console.log("true");
+    
+
+}
+    
+ let num = 12;
+  if((num%3== 0) && ((num=+1 == 15) || (num-1 == 11))){
+    console.log("safe");
+  }   else{
+    console.log("unsafe");
+    
+  }
+
+
+
+
+
+
+
+
+
+
